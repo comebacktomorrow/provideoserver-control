c7677e57e35f3b37218ae0782ae15275439bf4cc
David Tilley <davidtilley@Davids-MacBook-Pro-3.local>
2024-07-17 17:08:21 +1000

    Add tsl-umd to package.json and index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const ProVideoServerController = require('./ProVideoServerController');
 const startInteractiveConsole = require('./Interactive/interactive');
 const startWebServer = require('./REST/server');
 const { updateAllTimers } = require('./ProPresenter/ProPresenterTimers');
+const { umd } = require('./TSL-UMD/tsl-umd');
+//const { sendUMDMessage } = require('./TSL-UMD/tsl-umd-send'); 
+const goUMD = require('./TSL-UMD/tsl-umd');
 
 const PVS_IP_ADDRESS = '127.0.0.1'; // Replace with the actual IP address
 const PVS_PORT = 3811; // Replace with the actual port
@@ -12,7 +15,11 @@ const PVS_CHANNEL_NAME = 'Vtr1';
 const PRO_PRESENTER_IP = 'localhost'; // Replace with the actual IP address
 const PRO_PRESENTER_PORT = 50050; // Replace with the actual port
 
+const TSL_PORT = 40041; //That we're listening on
+const TSL_ADDRESS = 2; //That we're listening for
 const controller = new ProVideoServerController(PVS_IP_ADDRESS, PVS_PORT, PVS_CHANNEL_NUMBER, PVS_CHANNEL_NAME);
+
+goUMD(controller);
 
 // Start the interactive console
 startInteractiveConsole(controller);
