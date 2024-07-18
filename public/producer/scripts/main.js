dc51642e79fd0ab8a98581e028a8dff9a6083160
David Tilley <davidtilley@Davids-MacBook-Pro-4.local>
2024-07-18 13:00:47 +1000

    Change names for modularity

--- public/producer/scripts/main.js
+++
@@ -1,9 +0,0 @@
-import { setupWebSocket } from './websocket.js';
-import { fetchPlaylistData } from './playlist.js';
-
-const init = () => {
-    setupWebSocket();
-    fetchPlaylistData();
-};
-
-document.addEventListener('DOMContentLoaded', init);
